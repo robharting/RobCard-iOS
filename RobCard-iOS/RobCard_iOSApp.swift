864767bbdd2e8e20be60211a9ec1113741748a6e
Rob Harting <robharting@hotmail.com>
2024-09-18 10:19:43 +0200

    Initial Commit

---
+++ RobCard-iOS/RobCard_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  RobCard_iOSApp.swift
+//  RobCard-iOS
+//
+//  Created by Harting, R.P.G. (Rob) on 18/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct RobCard_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
