1bfba4980842cd1543f1440380c4460c74628da3
Rob Harting <robharting@hotmail.com>
2024-09-19 08:47:29 +0200

    created my business card with swiftUI

--- RobCard-iOS/ContentView.swift
+++ RobCard-iOS/ContentView.swift
@@ -9,16 +9,38 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            // #0652DD - Merchant Marine Blue from https://flatuicolors.com/palette/nl
+            Color(red: 0.02, green: 0.32, blue: 0.87, opacity: 1.0)
+                .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            VStack {
+                
+                VStack {
+                    Image("rob")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 200, height: 200)
+                        .clipShape(Circle())
+                        .overlay(
+                            Circle().stroke(Color.white, lineWidth: 5)
+                        )
+                    Text("Rob Harting")
+                        .font(Font.custom("Roboto-Bold", size: 40))
+                        .bold()
+                        .foregroundColor(.white)
+                    Text("iOS Developer & IT CL")
+                        .foregroundColor(.white)
+                        .font(.system(size: 25))
+                    Divider()
+                    InfoView(text: "+31 6 300 70 678", imageName: "phone.fill")
+                    InfoView(text: "robharting@hotmail.com", imageName: "envelope.fill")
+                }
+            }
         }
-        .padding()
     }
 }
 
 #Preview {
     ContentView()
 }
+
---
+++ RobCard-iOS/InfoView.swift
@@ -0,0 +1,35 @@
+//
+//  InfoView.swift
+//  RobCard-iOS
+//
+//  Created by Harting, R.P.G. (Rob) on 19/09/2024.
+//
+
+import SwiftUI
+
+struct InfoView: View {
+    
+    let text: String
+    let imageName: String
+    
+    var body: some View {
+        RoundedRectangle(cornerRadius: 10)
+            .fill(Color(.white))
+            .frame(height: 50)
+            .overlay(
+                HStack {
+                    Image(systemName: imageName)
+                        .foregroundColor(Color(red: 0.02, green: 0.32, blue: 0.87, opacity: 1.0))
+                    Text(text)
+                        .font(Font.custom("Roboto-Bold", size: 20))
+                        .bold()
+                })
+            .padding(.all)
+    }
+}
+
+
+#Preview {
+    InfoView(text:"hello", imageName: "phone.fill")
+        .previewLayout(.sizeThatFits)
+}
